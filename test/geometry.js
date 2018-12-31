f3645a4122e143513315708f1966941c78500ac4
ajlopez <webmaster@ajlopez.com>
2018-12-31 16:11:24 -0300

    Create line

--- test/geometry.js
+++ test/geometry.js
@@ -10,3 +10,17 @@ exports['create point'] = function (test) {
     test.equal(point.y(), 2);
 };
 
+exports['create line'] = function (test) {
+    var p1 = geometry.point(1, 2);
+    var p2 = geometry.point(3, 4);
+    
+    var line = geometry.line(p1, p2);
+    
+    test.ok(line);
+    test.equal(typeof line, 'object');
+    test.equal(line.from().x(), 1);
+    test.equal(line.from().y(), 2);
+    test.equal(line.to().x(), 3);
+    test.equal(line.to().y(), 4);
+};
+
